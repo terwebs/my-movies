181662199add38831fca2e39097c85741338b6e9
terwebs <terwebs88@gmail.com>
2023-12-04 12:10:27 -0600

    move movie to own component, add toggle to show/hide overview

---
+++ src/components/Movie.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+const imgPath = "https://image.tmdb.org/t/p/w1280"
+
+// eslint-disable-next-line react/prop-types
+export default function Movie({ movie }) {
+  // eslint-disable-next-line react/prop-types
+  let { title, poster_path, overview } = movie
+  const [isOverviewDisplayed, setisOverviewDisplayed] = useState(false)
+  const toggleOverviewDisplayed = () =>
+    setisOverviewDisplayed(!isOverviewDisplayed)
+
+  return (
+    <>
+      <li onClick={toggleOverviewDisplayed}>
+        <div className="movie-content">
+          <img
+            className="movie-poster"
+            src={imgPath + poster_path}
+            alt={title}
+          />
+          <div className={isOverviewDisplayed ? "overview show" : "overview"}>
+            <h3 className="overview-title">Overview</h3>
+            {overview}
+          </div>
+        </div>
+        <div className="movie-info">
+          <h3>{title}</h3>
+        </div>
+      </li>
+    </>
+  )
+}
